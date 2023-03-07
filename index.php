4818455a896b6f857440d96973fe8bf64d9cec84
hanshaugen <97103592+hanshaugen@users.noreply.github.com>
2023-03-07 15:50:53 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <?php
 
 //echo "Hello World!";
-$im = imagecreatefrompng(
-'https://github.com/hanshaugen/Skatt/blob/master/tresure.png');
+$im = imagecreatefrompng('tresure.png');
   
 // View the loaded image in browser using imagepng() function
 header('Content-type: image/png');  
